662187d52a9a3f79ec2da0e44d1e7909e3c711f1
Helga Kirilchik <kirilchik201146@gmail.com>
2021-01-28 16:05:06 +0300

    Beautify files

--- js/code.js
+++ js/code.js
@@ -1,39 +1,39 @@
-window.onload = function (){
+window.onload = function () {
     let result = {};
     let step = 0;
 
 
-function showQuestion(questionNumber){
-    document.querySelector(".question").innerHTML = quiz[step]['q'];
-let answer = '';
-for (let key in quiz[step]['a']){
-    answer+= `<li data-v="${key}" class="answer-variant">${quiz[step]['a'][key]}</li>`;
-}
-document.querySelector(".answer").innerHTML = answer;
+    function showQuestion(questionNumber) {
+        document.querySelector(".question").innerHTML = quiz[step]['q'];
+        let answer = '';
+        for (let key in quiz[step]['a']) {
+            answer += `<li data-v="${key}" class="answer-variant">${quiz[step]['a'][key]}</li>`;
+        }
+        document.querySelector(".answer").innerHTML = answer;
     }
-    document.onclick = function(event){
+    document.onclick = function (event) {
         event.stopPropagation();
-        if(event.target.classList.contains('answer-variant')&& step < quiz.length){
-           // console.log (event.target);
-        if (result[event.target.dataset.v] != undefined) {
-            result[event.target.dataset.v]++;
-        } else {
-            result[event.target.dataset.v] = 0;
+        if (event.target.classList.contains('answer-variant') && step < quiz.length) {
+            // console.log (event.target);
+            if (result[event.target.dataset.v] != undefined) {
+                result[event.target.dataset.v]++;
+            } else {
+                result[event.target.dataset.v] = 0;
+            }
+            step++;
+            if (step == quiz.length) {
+                document.querySelector('.question').remove();
+                document.querySelector('.answer').remove();
+                showResult();
+            } else {
+                showQuestion(step);
+            }
         }
-        step++;
-        if (step == quiz.length){
-            document.querySelector('.question').remove();
-            document.querySelector('.answer').remove();
-            showResult();
-        }
-        else {
-            showQuestion(step);
-        }
+        console.log(result);
     }
-    console.log(result);
-    }
-    function showResult(){
-        let key = Object.keys(result).reduce(function(a,b){
+
+    function showResult() {
+        let key = Object.keys(result).reduce(function (a, b) {
             return result[a] > result[b] ? a : b;
         });
         console.log(key);
